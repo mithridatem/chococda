77d2db7cf794ad0c76c59093448c698173ef8320
mithridatem <mathieumithridate@adrar-formation.com>
2024-04-15 13:40:08 +0200

    Feat: form controller register

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -2,17 +2,24 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegisterType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+
 class RegisterController extends AbstractController
 {
-    #[Route('/register', name: 'app_register')]
-    public function index(): Response
+    #[Route('/register', name: 'app_register_create')]
+    public function create(): Response
     {
+        $user = new User();
+        $form = $this->createForm(RegisterType::class,$user);
         return $this->render('register/index.html.twig', [
-            'controller_name' => 'RegisterController',
+            'formulaire' => $form->createView()
         ]);
     }
 }
