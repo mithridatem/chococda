f800786d4bcae578448bb9bb6d83d0006841c01d
mithridatem <mathieumithridate@adrar-formation.com>
2024-04-17 19:06:09 +0200

    update: css

--- src/Service/CommentaryService.php
+++ src/Service/CommentaryService.php
@@ -3,6 +3,7 @@
 namespace App\Service;
 
 use App\Repository\CommentaryRepository;
+use App\Repository\ChocoblastRepository;
 use Doctrine\ORM\EntityManagerInterface;
 
 class CommentaryService implements ServiceInterface
@@ -10,50 +11,67 @@ class CommentaryService implements ServiceInterface
 
     public function __construct(
         private readonly EntityManagerInterface $em,
-        private readonly CommentaryRepository $commentaryRepository
+        private readonly CommentaryRepository $commentaryRepository,
+        private readonly ChocoblastRepository $chocoblastRepository
     ) {
     }
     public function create(object $objet)
     {
-        if (!$this->commentaryRepository->findOneBy([
-            "content" => $objet->getContent(), "createAt" => $objet->getCreateAt()
-            ])) {
-                $this->em->persist($objet);
-                $this->em->flush();
-        }
-        else{
+        if (
+            !$this->commentaryRepository->findOneBy([
+                "content" => $objet->getContent(),
+                "createAt" => $objet->getCreateAt()
+            ])
+        ) {
+            $this->em->persist($objet);
+            $this->em->flush();
+        } else {
             throw new \Exception("Le commentaire existe déja");
         }
     }
 
-    public function  update(object $objet) {
-        if ($this->commentaryRepository->findOneBy([
-            "content" => $objet->getContent(), "createAt" => $objet->getCreateAt()
-            ])) {
-                $this->em->persist($objet);
-                $this->em->flush();
-        }
-        else{
+    public function update(object $objet)
+    {
+        if (
+            $this->commentaryRepository->findOneBy([
+                "content" => $objet->getContent(),
+                "createAt" => $objet->getCreateAt()
+            ])
+        ) {
+            $this->em->persist($objet);
+            $this->em->flush();
+        } else {
             throw new \Exception("Le commentaire n'existe pas");
         }
     }
 
-    public function delete(int $id) {
+    public function delete(int $id)
+    {
         $commentary = $this->commentaryRepository->find($id);
         if ($commentary) {
             $this->em->remove($commentary);
             $this->em->flush();
-        }
-        else{
+        } else {
             throw new \Exception("le commentaire n'existe pas");
         }
     }
 
-    public function findOneBy(int $id): object {
-        return $this->commentaryRepository->find($id)??throw new \Exception("Le commentaire n'existe pas");
+    public function findOneBy(int $id): object
+    {
+        return $this->commentaryRepository->find($id)
+            ?? throw new \Exception("Le commentaire n'existe pas");
     }
 
-    public function findAll(): array {
-        return $this->commentaryRepository->findAll()??throw new \Exception("La liste des commentaires est vide");
+    public function findAll(): array
+    {
+        return $this->commentaryRepository->findAll()
+            ?? throw new \Exception("La liste des commentaires est vide");
+    }
+
+    public function findAllByChocoblastId(int $id): array
+    {
+        return $this->commentaryRepository->findBy(
+            ["chocoblast" => $this->chocoblastRepository->find($id)]
+        ) ?? throw new \Exception("Aucun commentaire associé au chocoblast");
     }
 }
--- src/Service/EmailService.php
+++ src/Service/EmailService.php
@@ -6,29 +6,32 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-class EmailService{
+class EmailService
+{
     private string $login;
     private string $password;
     private string $server;
     private int $port;
     private PHPMailer $mail;
 
-    public function __construct(string $login, string $password, string $server, int $port) {
+    public function __construct(string $login, string $password, string $server, int $port)
+    {
         $this->login = $login;
         $this->password = $password;
         $this->server = $server;
         $this->port = $port;
         $this->mail = new PHPMailer(true);
     }
-    public function sendEmail(string $receiver, string $object, string $body){
+    public function sendEmail(string $receiver, string $object, string $body)
+    {
         try {
             $this->config();
             //Recipients
             $this->mail->setFrom($this->login);
-            $this->mail->addAddress($receiver); 
- 
+            $this->mail->addAddress($receiver);
+
             //Content
-            $this->mail->isHTML(true);                                  
+            $this->mail->isHTML(true);
             $this->mail->Subject = $object;
             $this->mail->Body    = $body;
             $this->mail->send();
@@ -37,7 +40,8 @@ class EmailService{
             echo "Message could not be sent. Mailer Error: {$this->mail->ErrorInfo}";
         }
     }
-    public function config() {
+    public function config()
+    {
         //Server settings
         $this->mail->SMTPDebug = SMTP::DEBUG_OFF;
         $this->mail->isSMTP();
