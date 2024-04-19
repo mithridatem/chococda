e9cb652456a38cc86c6cf0ec1d43d16369302393
mithridatem <mathieumithridate@adrar-formation.com>
2024-04-19 11:14:51 +0200

    fix : message connecté

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,22 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://127.0.0.1:8000/register')
+    cy.get('#register_firstname').type("Mathieu")
+    cy.get('#register_lastname').type("Mithridate")
+    cy.get('#register_email').type("mathieu.mith3@laposte.net")
+    cy.get('#register_password').type("Azertyuio1234")
+    cy.get('[type="submit"]').click()
+    cy.wait(2000)
+    cy.get('p.alert').should('contain', "Le compte a ete ajoute")
+  })
+  it('passes', () => {
+    cy.visit('https://127.0.0.1:8000/register')
+    cy.get('#register_firstname').type("Mathieu")
+    cy.get('#register_lastname').type("Mithridate")
+    cy.get('#register_email').type("mathieu.mith3@laposte.net")
+    cy.get('#register_password').type("Azertyuio1234")
+    cy.get('[type="submit"]').click()
+    cy.wait(2000)
+    cy.get('p.alert').should('contain' ,"Le compte existe deja")
+  })
+})
---
+++ exemple.cy.js
@@ -0,0 +1,5 @@
+describe('exemple', () => {
+    it('passes', () => {
+        cy.visit('https://localhost:8000/register/add')
+    })
+})
